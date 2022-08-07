a378fa5ec4fc5686c85506ac8d8873158c70d412
richardgrif <103006344+richardgrif@users.noreply.github.com>
2022-08-07 14:34:34 -0500

    Initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
