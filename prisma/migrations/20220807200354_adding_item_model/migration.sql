5d485447e30a0a2ea1fc3eca3b8ba0a93e150d7f
richardgrif <103006344+richardgrif@users.noreply.github.com>
2022-08-07 15:43:21 -0500

    Created form to add items to db

---
+++ prisma/migrations/20220807200354_adding_item_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Item` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `rating` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
